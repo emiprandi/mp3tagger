7922cce527879ca367af52520046e84984e2dde2
Emiliano Prandi <emiprandi@gmail.com>
2015-04-13 12:30:43 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+## About mp3tagger
+
+`mp3tagger` is a simple command line tool that search album info (such as title, artist, track names, etc.) and write id3 tags to mp3 files. Perfect for your iTunes collection. It uses [Discogs.com](http://www.discogs.com) database and [eyeD3](http://eyed3.nicfit.net/) for write id3v2 metadata.
+
+## Requirements
+
+### node & npm
+
+### eyeD3
+* To install eyeD3 cli: `brew install eye-d3`
+
+### discogs.com account
+* Go to [discogs.com/developers](http://www.discogs.com/developers) and "Create an app".
+* Add the keys to your `.bash_profile`:
+
+    export DISCOGS_CONSUMER_KEY=<your_key>
+    export DISCOGS_CONSUMER_SECRET=<your_secret>
+
+## Installation
+* `npm install -g mp3tagger`
+
+## Usage
+* Go to and album directory with mp3s inside and type `mp3tagger`
+
+## Notes
+* mp3tagger will search for files in the directory by alphabetical order
+* only tested in mac
---
+++ app.js
@@ -0,0 +1,141 @@
+#!/usr/bin/env node
+
+var fs = require('fs'),
+    dir = process.cwd(),
+    mp3s = [],
+    brain = {
+        query: {},
+        options: [],
+        data: {
+            artist: '',
+            album: '',
+            year: '',
+            tracks: []
+        }
+    },
+    eD3c, eD3p,
+    readline = require('readline'),
+    Discogs = require('disconnect').Client,
+    spawn = require('child_process').spawn;
+
+function error (msg) {
+    console.log(msg + '\n');
+}
+
+if (process.env.DISCOGS_CONSUMER_KEY && process.env.DISCOGS_CONSUMER_SECRET) {
+    var dcApi = {
+        key: process.env.DISCOGS_CONSUMER_KEY,
+        secret: process.env.DISCOGS_CONSUMER_SECRET
+    };
+} else {
+    return error('We couldn\'t find Discogs keys.');
+}
+
+fs.readdir(dir, function (err, files) {
+    if (err) return error('blop!');
+
+    files.forEach(function (file) {
+        if (file.substr(-4) === '.mp3') {
+            mp3s.push(file);
+        }
+    });
+
+    if (mp3s.length === 0) {
+        return error('No mp3s to tag.');
+    } else {
+        eD3c = spawn('which', ['eyeD3']);
+        eD3c.on('close', function (exitcode) {
+            if (exitcode === 0) {
+                var rl = readline.createInterface({
+                    input: process.stdin,
+                    output: process.stdout
+                });
+
+                console.log(' .-.     .-.     .-.     .-.     .-.     .-.     .-.     .-.');
+                console.log("'   `._.'   `._.'   `._.'   `._.'   `._.'   `._.'   `._.'   `");
+                console.log('mp3tagger');
+                console.log('     .-.     .-.     .-.     .-.     .-.     .-.     .-.');
+                console.log("`._.'   `._.'   `._.'   `._.'   `._.'   `._.'   `._.'   `._.'");
+                console.log(dir);
+                console.log('MP3s found: ' + mp3s.length);
+                console.log('');
+                rl.question('Artist?: ', function (answer) {
+                    brain.query.artist = answer;
+
+                    rl.question('Album?: ', function (answer) {
+                        brain.query.album = answer;
+
+                        console.log('Searching...\n');
+
+                        var dc = new Discogs({ consumerKey: dcApi.key, consumerSecret: dcApi.secret }).database();
+
+                        dc.search('', { type: 'master', artist: brain.query.artist, release_title: brain.query.album }, function (err, data) {
+                            if (data.results.length > 0) {
+                                data.results.forEach(function (item, i) {
+                                    brain.options.push(item.id);
+                                    console.log('[' + i + ']: ' + item.title);
+                                    console.log(item.format);
+                                    console.log('-----------');
+                                });
+
+                                rl.question('Choose an album: ', function (answer) {
+                                    console.log('Fetching album data...\n');
+
+                                    dc.master(brain.options[parseInt(answer)], function (err, data) {
+                                        brain.data.artist = data.artists[0].name;
+                                        brain.data.album = data.title;
+                                        brain.data.year = data.year;
+
+                                        console.log(data.title + ' (' + data.year + ')');
+                                        console.log(data.tracklist.length + ' tracks');
+                                        console.log('ID: ' + data.id + ' - Data: ' + data.data_quality);
+                                        console.log('----------------------');
+
+                                        data.tracklist.forEach(function (track, i) {
+                                            brain.data.tracks.push(track.title);
+                                            console.log(track.position + '. ' + track.title + ' (' + track.duration + ')');
+                                        });
+
+                                        rl.question('\nWrite ID3 tags? [y,n]: ', function (answer) {
+                                            if (answer === "y") {
+                                                mp3s.forEach(function (file, i) {
+                                                    var args = [];
+                                                    args.push('--remove-all');
+                                                    args.push('-a', brain.data.artist);
+                                                    args.push('-A', brain.data.album);
+                                                    args.push('-t', brain.data.tracks[i]);
+                                                    args.push('-n', (i+1));
+                                                    args.push('-N', brain.data.tracks.length);
+                                                    //args.push('-d', '1');
+                                                    //args.push('-D', '1');
+                                                    args.push('-Y', brain.data.year);
+                                                    args.push('--recording-date', brain.data.year);
+                                                    args.push(file);
+                                                    eD3p = spawn('eyeD3', args);
+
+                                                    eD3p.on('close', function (exitcode) {
+                                                        if (exitcode === 0) {
+                                                            console.log('[OK] ' + file);
+                                                        } else {
+                                                            console.log('[ERR] ' + file);
+                                                        }
+                                                    });
+                                                });
+                                            }
+                                            rl.close();
+                                        });
+                                    });
+                                });
+                            } else {
+                                console.log('Sorry, we couldn\'t find album info.');
+                                rl.close();
+                            }
+                        });
+                    });
+                });
+            } else {
+                return error('Sorry, this script requires eyeD3. Try `brew install eyeD3` first.');
+            }
+        });
+    }
+});
